61f94ef6e2c6411d6194783cb1a690d2ac363bf0
oshrit-feruz <oshrit13200@gmail.com>
2022-10-19 16:36:30 +0300

    e

--- deploy/server/server.js
+++ deploy/server/server.js
@@ -27,8 +27,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const process_1 = __importDefault(require("process"));
+// import cors from 'cors'
 const dbAdmin_1 = require("./dbAdmin");
 const getDb_1 = __importStar(require("./getDb"));
+// import { insertNewUser } from "./dbAdmin";
+// import { getUserOrder } from './getDb';
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const express = require('express');
@@ -37,10 +40,11 @@ const port = process_1.default.env.PORT || 3002;
 const app = express();
 app.use(jsonParser);
 app.use(bodyParser.json());
+// app.use(cors())
 if (process_1.default.env.NODE_ENV === 'production') {
     app.use(express.static('build'));
-    app.get('*', (_req, res) => {
-        res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    app.get('/', (_req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
     });
 }
 app.get('/getRestaurants', (_req, res) => {
@@ -66,12 +70,35 @@ app.get('/restaurant/:restaurantName', (req, res) => {
     console.log("insert");
     (0, getDb_1.dishesByRestaurant)(restaurantName).then((dishes) => res.json(dishes));
 });
+// app.get('/getUser/:number', (req: any, response: any) => {
+//   const number = Number(req.params.number);
+//   getUserData(number).then((user) => response.json(user));
+// });
 app.post('/insertUser', (request, response) => {
     const userData = request.body;
     console.log(userData + "res from server");
     (0, dbAdmin_1.insertNewUser)(userData);
     response.send(({ response: 'you succsess!' }));
 });
+// app.get('/getOrders/:number', (req: any, response: any) => {
+//   const number = Number(req.params.number);
+//   getUserOrder(number).then((orders) => response.json(orders));
+// });
+// app.post('/addToUsers', (request: any, response:any) => {
+//   const userData = request.body;
+//   // addUser(userData)
+//   response.send({ word: 'you succsess!' })
+// });
+// app.delete('/deleteLocationHistory:number', (req: any, _res: any) => {
+//   const number = Number(req.params.number);
+//   deleteLocationHistory(number)
+// })
+// app.get('/locations-history', function (_req: any, res: any) { // serve main path as static file
+//   res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
+// app.get('/add-new', function (_req: any, res: any) { // serve main path as static file
+//   res.sendFile(path.join(__dirname, '../client/admin.html'));
+// });
 app.listen(port || 3002, () => {
     console.log('listen to port ' + port);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,19 +1,24 @@
 import process from "process";
+// import cors from 'cors'
 import { checkUser, insertNewUser } from "./dbAdmin";
 import getRestaurants, { dishesByRestaurant, getByCategory, getDesserts, getFavorites } from "./getDb";
+// import { insertNewUser } from "./dbAdmin";
+// import { getUserOrder } from './getDb';
+
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
+
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3002;
 const app = express();
 app.use(jsonParser);
 app.use(bodyParser.json());
-
+// app.use(cors())
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('build'))
-  app.get('*', (_req: any, res: any) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'))
+  app.get('/', (_req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
   })
 }
 app.get('/getRestaurants', (_req: Request, res: any) => {
@@ -40,12 +45,39 @@ app.get('/restaurant/:restaurantName', (req: any, res: any) => {
 
   dishesByRestaurant(restaurantName).then((dishes) => res.json(dishes))
 })
+// app.get('/getUser/:number', (req: any, response: any) => {
+//   const number = Number(req.params.number);
+//   getUserData(number).then((user) => response.json(user));
+// });
 app.post('/insertUser', (request: any, response: any) => {
   const userData = request.body;
   console.log(userData + "res from server");
   insertNewUser(userData)
   response.send(({ response: 'you succsess!' }))
 })
+// app.get('/getOrders/:number', (req: any, response: any) => {
+//   const number = Number(req.params.number);
+//   getUserOrder(number).then((orders) => response.json(orders));
+// });
+// app.post('/addToUsers', (request: any, response:any) => {
+//   const userData = request.body;
+//   // addUser(userData)
+//   response.send({ word: 'you succsess!' })
+// });
+// app.delete('/deleteLocationHistory:number', (req: any, _res: any) => {
+//   const number = Number(req.params.number);
+//   deleteLocationHistory(number)
+// })
+
+
+// app.get('/locations-history', function (_req: any, res: any) { // serve main path as static file
+//   res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
+
+// app.get('/add-new', function (_req: any, res: any) { // serve main path as static file
+//   res.sendFile(path.join(__dirname, '../client/admin.html'));
+// });
+
 app.listen(port || 3002, () => {
   console.log('listen to port ' + port);
 });
