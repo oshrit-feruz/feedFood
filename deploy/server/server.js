12bd43a15c33c2d16816af5540726758922985aa
oshrit-feruz <oshrit13200@gmail.com>
2022-10-19 13:41:14 +0300

    DE

--- deploy/server/server.js
+++ deploy/server/server.js
@@ -27,8 +27,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const process_1 = __importDefault(require("process"));
+// import cors from 'cors'
 const dbAdmin_1 = require("./dbAdmin");
 const getDb_1 = __importStar(require("./getDb"));
+// import { insertNewUser } from "./dbAdmin";
+// import { getUserOrder } from './getDb';
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const express = require('express');
@@ -37,29 +40,32 @@ const port = process_1.default.env.PORT || 3002;
 const app = express();
 app.use(jsonParser);
 app.use(bodyParser.json());
-// app.use(express.static('build'))
-// app.get('*', (_req: any, res: any) => {
-//   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-// })
-app.get('/getRestaurants', (_req, res) => {
+// app.use(cors())
+if (process_1.default.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+    app.get('https://feed-food.herokuapp.com/*', (_req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
+}
+app.get('https://feed-food.herokuapp.com/getRestaurants', (_req, res) => {
     (0, getDb_1.default)().then((restaurants) => res.json(restaurants));
 });
-app.get('/getFavorites', (_req, res) => {
+app.get('https://feed-food.herokuapp.com/getFavorites', (_req, res) => {
     (0, getDb_1.getFavorites)().then((restaurants) => res.json(restaurants));
 });
-app.get('/getDesserts', (_req, res) => {
+app.get('https://feed-food.herokuapp.com/getDesserts', (_req, res) => {
     (0, getDb_1.getDesserts)().then((restaurants) => res.json(restaurants));
 });
-app.post('/checkUser', (req, res) => {
+app.post('https://feed-food.herokuapp.com/checkUser', (req, res) => {
     const userData = req.body;
     console.log(userData);
     (0, dbAdmin_1.checkUser)(userData).then((userDb) => res.json(userDb));
 });
-app.get('/getByCategory/:category', (req, res) => {
+app.get('https://feed-food.herokuapp.com/getByCategory/:category', (req, res) => {
     let category = req.params.category;
     (0, getDb_1.getByCategory)(category).then((restaurants) => res.json(restaurants));
 });
-app.get('/restaurant/:restaurantName', (req, res) => {
+app.get('https://feed-food.herokuapp.com/restaurant/:restaurantName', (req, res) => {
     let restaurantName = req.params.restaurantName;
     console.log(req);
     console.log("insert");
@@ -69,7 +75,7 @@ app.get('/restaurant/:restaurantName', (req, res) => {
 //   const number = Number(req.params.number);
 //   getUserData(number).then((user) => response.json(user));
 // });
-app.post('/insertUser', (request, response) => {
+app.post('https://feed-food.herokuapp.com/insertUser', (request, response) => {
     const userData = request.body;
     console.log(userData + "res from server");
     (0, dbAdmin_1.insertNewUser)(userData);
@@ -94,12 +100,6 @@ app.post('/insertUser', (request, response) => {
 // app.get('/add-new', function (_req: any, res: any) { // serve main path as static file
 //   res.sendFile(path.join(__dirname, '../client/admin.html'));
 // });
-if (process_1.default.env.NODE_ENV === 'production') {
-    app.use(express.static('build'));
-    app.get('*', (_req, res) => {
-        res.sendFile(path.join(__dirname, '../build', 'index.html'));
-    });
-}
 app.listen(port || 3002, () => {
     console.log('listen to port ' + port);
 });
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,39 +1,45 @@
 import process from "process";
+// import cors from 'cors'
 import { checkUser, insertNewUser } from "./dbAdmin";
 import getRestaurants, { dishesByRestaurant, getByCategory, getDesserts, getFavorites } from "./getDb";
+// import { insertNewUser } from "./dbAdmin";
+// import { getUserOrder } from './getDb';
 
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json();
+
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3002;
 const app = express();
 app.use(jsonParser);
 app.use(bodyParser.json());
-
-// app.use(express.static('build'))
-// app.get('*', (_req: any, res: any) => {
-//   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-// })
-app.get('/getRestaurants', (_req: Request, res: any) => {
+// app.use(cors())
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'))
+  app.get('https://feed-food.herokuapp.com/*', (_req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+  })
+}
+app.get('https://feed-food.herokuapp.com/getRestaurants', (_req: Request, res: any) => {
   getRestaurants().then((restaurants) => res.json(restaurants))
 })
-app.get('/getFavorites', (_req: Request, res: any) => {
+app.get('https://feed-food.herokuapp.com/getFavorites', (_req: Request, res: any) => {
   getFavorites().then((restaurants) => res.json(restaurants))
 })
-app.get('/getDesserts', (_req: Request, res: any) => {
+app.get('https://feed-food.herokuapp.com/getDesserts', (_req: Request, res: any) => {
   getDesserts().then((restaurants) => res.json(restaurants))
 })
-app.post('/checkUser', (req: any, res: any) => {
+app.post('https://feed-food.herokuapp.com/checkUser', (req: any, res: any) => {
   const userData = req.body;
   console.log(userData)
   checkUser(userData).then((userDb) => res.json(userDb))
 })
-app.get('/getByCategory/:category', (req: any, res: any) => {
+app.get('https://feed-food.herokuapp.com/getByCategory/:category', (req: any, res: any) => {
   let category = req.params.category;
   getByCategory(category).then((restaurants) => res.json(restaurants))
 })
-app.get('/restaurant/:restaurantName', (req: any, res: any) => {
+app.get('https://feed-food.herokuapp.com/restaurant/:restaurantName', (req: any, res: any) => {
   let restaurantName = req.params.restaurantName;
   console.log(req);
   console.log("insert");
@@ -44,13 +50,12 @@ app.get('/restaurant/:restaurantName', (req: any, res: any) => {
 //   const number = Number(req.params.number);
 //   getUserData(number).then((user) => response.json(user));
 // });
-app.post('/insertUser', (request: any, response: any) => {
+app.post('https://feed-food.herokuapp.com/insertUser', (request: any, response: any) => {
   const userData = request.body;
   console.log(userData + "res from server");
   insertNewUser(userData)
   response.send(({ response: 'you succsess!' }))
 })
-
 // app.get('/getOrders/:number', (req: any, response: any) => {
 //   const number = Number(req.params.number);
 //   getUserOrder(number).then((orders) => response.json(orders));
@@ -73,12 +78,6 @@ app.post('/insertUser', (request: any, response: any) => {
 // app.get('/add-new', function (_req: any, res: any) { // serve main path as static file
 //   res.sendFile(path.join(__dirname, '../client/admin.html'));
 // });
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
-  app.get('*', (_req: any, res: any) => {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-  });
-}
 
 app.listen(port || 3002, () => {
   console.log('listen to port ' + port);
